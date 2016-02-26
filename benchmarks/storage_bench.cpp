4f29e782d2a5db648e194d6a9e635c8d9f503143
Sergey Lyubimov <lysevi@gmail.com>
2016-02-26 11:04:21 +0300

    storeage_bench.

--- benchmarks/storage_bench.cpp
+++ benchmarks/storage_bench.cpp
@@ -14,18 +14,21 @@ int main(int argc, char *argv[]) {
     {
 		auto ms = new memseries::storage::MemoryStorage{ 100000 };
 		auto m = memseries::Meas::empty();
-		
+
 		std::vector<memseries::Time> deltas{ 50,255,1024,2050 };
 		memseries::Time t = 1;
 		const size_t ids_count = 10;
-        auto start=clock();
-		for (auto i = 0; i < 1000000; i ++) {
-            m.id = i%ids_count;
-			m.flag = 0xff;
-			m.time=t+deltas[i%deltas.size()];
-			auto sv = (std::sin(i));
-			m.value = *(memseries::Id*)(&sv);
-			ms->append(m);
+
+		auto start = clock();
+		for (int K = 0; K < 5; K++) {
+			
+			for (auto i = 0; i < 1000000; i++) {
+				m.id = i%ids_count;
+				m.flag = 0xff;
+				m.time = t + deltas[i%deltas.size()];
+				m.value = K+i/10.0;
+				ms->append(m);
+			}
 		}
         auto elapsed=((float)clock()-start)/ CLOCKS_PER_SEC;
         std::cout<<"memorystorage insert : "<<elapsed<<std::endl;
