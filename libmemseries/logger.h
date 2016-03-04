3a1ce8a42c9d835c9e2cefc60498162cf60cbe8f
Sergey Lyubimov <lysevi@gmail.com>
2016-03-04 13:05:52 +0300

    logger refact.

--- libmemseries/logger.h
+++ libmemseries/logger.h
@@ -3,26 +3,6 @@
 #include <cstdint>
 #include <iostream>
 
-#define logger(msg)       memseries::utils::Logger::get()->GetStream()<<"   "<<msg<<std::endl
-#define logger_info(msg)  memseries::utils::Logger::get()->GetStream()<<"   "<<msg<<std::endl
-#define logger_fatal(msg) memseries::utils::Logger::get()->GetStream()<<"   "<<msg<<std::endl
-
-namespace memseries
-{
-	namespace utils
-	{
-		class Logger
-		{
-		public:
-			static Logger* get();
-			virtual std::ostream& GetStream();
-
-			virtual ~Logger() {
-			};
-		private:
-			uint16_t m_level;
-			Logger() {
-			};
-		};
-	}
-}
+#define logger(msg)       std::cout<<"    "<<msg<<std::endl
+#define logger_info(msg)  std::cout<<"[inf]"<<msg<<std::endl
+#define logger_fatal(msg) std::cerr<<"[err]"<<msg<<std::endl
