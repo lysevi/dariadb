73f665bf7cac32098da069e124716d577a18c2da
Sergey Lyubimov <lysevi@gmail.com>
2016-03-04 13:07:30 +0300

    indent.

--- libmemseries/logger.h
+++ libmemseries/logger.h
@@ -3,6 +3,6 @@
 #include <cstdint>
 #include <iostream>
 
-#define logger(msg)       std::cout<<"    "<<msg<<std::endl
+#define logger(msg)       std::cout<<"     "<<msg<<std::endl
 #define logger_info(msg)  std::cout<<"[inf]"<<msg<<std::endl
 #define logger_fatal(msg) std::cerr<<"[err]"<<msg<<std::endl
