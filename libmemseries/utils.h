6b9633de79e3f350b573f21854fe0803b0753cd8
Sergey Lyubimov <lysevi@gmail.com>
2016-03-04 16:51:24 +0300

    remove FS commands.

--- libmemseries/utils.h
+++ libmemseries/utils.h
@@ -3,7 +3,6 @@
 #include <string>
 #include <list>
 #include <iterator>
-#include <boost/filesystem.hpp>
 
 #define NOT_IMPLEMENTED throw std::logic_error("Not implemented");
 
@@ -52,13 +51,6 @@ namespace memseries {
 		protected:
 			NonCopy() = default;
 		};
-
-		std::list<boost::filesystem::path> ls(const std::string &path);
-		std::list<boost::filesystem::path> ls(const std::string &path,
-			const std::string &ext);
-		bool rm(const std::string &rm_path);
-		std::string filename(std::string fname); // without ex
-		std::string parent_path(std::string fname);
 
 		template <typename T> bool inInterval(T from, T to, T value) {
 			return value >= from && value <= to;
--- tests/utils_test.cpp
+++ tests/utils_test.cpp
@@ -1,20 +1,14 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Main
 #include <boost/test/unit_test.hpp>
-
 #include <utils.h>
+
 BOOST_AUTO_TEST_CASE(UtilsEmpty) {
   BOOST_CHECK(memseries::utils::inInterval(1, 5, 1));
   BOOST_CHECK(memseries::utils::inInterval(1, 5, 2));
   BOOST_CHECK(memseries::utils::inInterval(1, 5, 5));
   BOOST_CHECK(!memseries::utils::inInterval(1, 5, 0));
   BOOST_CHECK(!memseries::utils::inInterval(0, 1, 2));
-}
-
-BOOST_AUTO_TEST_CASE(FileUtils) {
-  std::string filename = "foo/bar/test.txt";
-  BOOST_CHECK_EQUAL(memseries::utils::filename(filename), "test");
-  BOOST_CHECK_EQUAL(memseries::utils::parent_path(filename), "foo/bar");
 }
 
 BOOST_AUTO_TEST_CASE(BitOperations) {
