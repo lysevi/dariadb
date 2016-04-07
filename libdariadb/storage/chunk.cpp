977c620c007725ad88b4d0b38456b26150baa68f
Sergey Lyubimov <lysevi@gmail.com>
2016-04-07 13:31:58 +0300

    chunk assert.

--- libdariadb/storage/chunk.cpp
+++ libdariadb/storage/chunk.cpp
@@ -93,7 +93,7 @@ Chunk::Chunk(const ChunkIndexInfo&index, const uint8_t* buffer, const size_t buf
 	}
 
 	range = Range{ _buffer_t.data(),_buffer_t.data() + buffer_length };
-	assert(size_t(range.end - range.begin), buffer_length);
+	assert(size_t(range.end - range.begin)==buffer_length);
 	bw = std::make_shared<BinaryBuffer>(range);
 	bw->set_bitnum(bw_bit_num);
 	bw->set_pos(bw_pos);
