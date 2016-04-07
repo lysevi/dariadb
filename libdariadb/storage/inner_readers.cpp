0f5226291f2ac7b62a1631a93d4c3c440fe9bde3
Sergey Lyubimov <lysevi@gmail.com>
2016-04-07 15:17:09 +0300

    verbose

--- libdariadb/storage/inner_readers.cpp
+++ libdariadb/storage/inner_readers.cpp
@@ -111,6 +111,10 @@ void InnerReader::readTimePoint(storage::ReaderClb*clb) {
         for (size_t i = 0; i < ch->count; i++) {
 
 			auto sub = crr.read();
+			std::cout << "sub: t" << sub.time
+				<< " v: " << sub.value
+				<< " f: " << sub.flag
+				<< " s: " << sub.src << std::endl;
             sub.id = ch->first.id;
 			if ((sub.time <= _from) && (sub.time >= candidate.time)) {
 				candidate = sub;
