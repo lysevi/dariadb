7f7092bed5b27bac7d4009dda955df6fccaf9ebe
Sergey Lyubimov <lysevi@gmail.com>
2016-05-17 08:03:03 +0300

    Query hasher.

--- libdariadb/storage/memstorage.h
+++ libdariadb/storage/memstorage.h
@@ -2,10 +2,50 @@
 
 #include "../storage.h"
 #include "chunk.h"
+#include <functional>
 
 namespace dariadb {
 namespace storage {
 
+struct IdArrayHasher {
+  std::size_t operator()(const dariadb::IdArray &ids) const {
+    size_t result = 0;
+    size_t num = 0;
+    std::hash<dariadb::Id> id_hasher{};
+    for (auto id : ids) {
+      result ^= (id_hasher(id) << num);
+      num++;
+    }
+    return result;
+  }
+};
+
+struct QueryIntervalHasher {
+  IdArrayHasher id_hasher;
+  std::hash<dariadb::Time> time_hasher;
+  std::hash<dariadb::Flag> flag_hasher;
+  std::size_t operator()(const dariadb::storage::QueryInterval &qi) const {
+    size_t result = 0;
+    result = id_hasher(qi.ids);
+    result ^= time_hasher(qi.from) << 1;
+    result ^= time_hasher(qi.to) << 2;
+    result ^= flag_hasher(qi.flag) << 3;
+    return result;
+  }
+};
+
+struct QueryTimePointHasher {
+  IdArrayHasher id_hasher;
+  std::hash<dariadb::Time> time_hasher;
+  std::hash<dariadb::Flag> flag_hasher;
+  std::size_t operator()(const dariadb::storage::QueryTimePoint &qp) const {
+    size_t result = 0;
+    result = id_hasher(qp.ids);
+    result ^= time_hasher(qp.time_point) << 1;
+    result ^= flag_hasher(qp.flag) << 2;
+    return result;
+  }
+};
 class MemoryStorage : public MeasSource {
 public:
   MemoryStorage();
