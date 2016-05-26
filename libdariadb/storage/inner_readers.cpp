18324603a0d13c87a880a3bb7aff34abc4bd2c56
Sergey Lyubimov <lysevi@gmail.com>
2016-05-26 08:02:19 +0300

    inner_readers refact.

--- libdariadb/storage/inner_readers.cpp
+++ libdariadb/storage/inner_readers.cpp
@@ -14,8 +14,7 @@ public:
 };
 
 InnerReader::InnerReader(dariadb::Flag flag, dariadb::Time from, dariadb::Time to)
-    : _cursors{}, _flag(flag), _from(from), _to(to), _tp_readed(false) {
-  is_time_point_reader = false;
+    : _cursors{}, _flag(flag), _from(from), _to(to) {
   end = false;
 }
 
@@ -24,13 +23,8 @@ void InnerReader::add(Cursor_ptr c) {
   this->_cursors.push_back(c);
 }
 
-void InnerReader::add_tp(Chunk_Ptr c) {
-  std::lock_guard<std::mutex> lg(_locker);
-  this->_tp_chunks[c->info->first.id].push_back(c);
-}
-
 bool InnerReader::isEnd() const {
-  return this->end && this->_tp_readed;
+  return this->end;
 }
 
 dariadb::IdArray InnerReader::getIds() const {
@@ -40,9 +34,6 @@ dariadb::IdArray InnerReader::getIds() const {
 void InnerReader::readNext(storage::ReaderClb *clb) {
   std::lock_guard<std::mutex> lg(_locker);
 
-  if (!_tp_readed) {
-    this->readTimePoint(clb);
-  }
   std::shared_ptr<CursorReader> reader_clbk{new CursorReader};
   for (auto id : this->_ids) {
     for (auto ch : _cursors) {
@@ -70,67 +61,9 @@ void InnerReader::readNext(storage::ReaderClb *clb) {
   end = true;
 }
 
-void InnerReader::readTimePoint(storage::ReaderClb *clb) {
-  std::lock_guard<std::mutex> lg(_locker_tp);
-  std::list<Chunk_Ptr> to_read_chunks{};
-  for (auto cand_ch : _tp_chunks) {
-    auto candidate = cand_ch.second.front();
-
-    for (auto cur_chunk : cand_ch.second) {
-      if (candidate->info->first.time < cur_chunk->info->first.time) {
-        candidate = cur_chunk;
-      }
-    }
-    to_read_chunks.push_back(candidate);
-  }
-
-  for (auto ch : to_read_chunks) {
-
-    auto bw = std::make_shared<BinaryBuffer>(ch->bw->get_range());
-    bw->reset_pos();
-    CopmressedReader crr(bw, ch->info->first);
-
-    Meas candidate = Meas::empty();
-    bool found = false;
-    // candidate = ch->info->first;
-    ch->lock();
-    for (size_t i = 0; i < ch->info->count; i++) {
-
-      auto sub = crr.read();
-      sub.id = ch->info->first.id;
-      if ((sub.time <= _from) && (sub.time >= candidate.time) &&
-          (sub.id == _ids.front())) {
-        candidate = sub;
-        found = true;
-      }
-      if (sub.time > _from) {
-        break;
-      }
-    }
-    ch->unlock();
-    if (found && candidate.time <= _from) {
-      // TODO make as options
-      candidate.time = _from;
-
-      clb->call(candidate);
-      _tp_readed_times.insert(std::make_tuple(candidate.id, candidate.time));
-    }
-  }
-  auto m = dariadb::Meas::empty();
-  m.time = _from;
-  m.flag = dariadb::Flags::_NO_DATA;
-  for (auto id : _not_exist) {
-    m.id = id;
-    clb->call(m);
-  }
-  _tp_readed = true;
-}
-
 bool InnerReader::check_meas(const Meas &m) const {
   auto tmp = std::make_tuple(m.id, m.time);
-  if (this->_tp_readed_times.find(tmp) != _tp_readed_times.end()) {
-    return false;
-  }
+  
   using utils::inInterval;
 
   if (m.inFlag(_flag) && (m.inInterval(_from, _to))) {
@@ -142,20 +75,14 @@ bool InnerReader::check_meas(const Meas &m) const {
 Reader_ptr InnerReader::clone() const {
   auto res = std::make_shared<InnerReader>(_flag, _from, _to);
   res->_cursors = _cursors;
-  res->_tp_chunks = _tp_chunks;
   res->_flag = _flag;
   res->_from = _from;
   res->_to = _to;
-  res->_tp_readed = _tp_readed;
   res->end = end;
-  res->_not_exist = _not_exist;
-  res->_tp_readed_times = _tp_readed_times;
   return res;
 }
 void InnerReader::reset() {
   end = false;
-  _tp_readed = false;
-  _tp_readed_times.clear();
   for (auto ch : _cursors) {
     ch->reset_pos();
   }
--- libdariadb/storage/inner_readers.h
+++ libdariadb/storage/inner_readers.h
@@ -17,33 +17,24 @@ public:
 
   void add(Cursor_ptr c);
 
-  void add_tp(Chunk_Ptr c);
   bool isEnd() const override;
 
   dariadb::IdArray getIds() const override;
   void readNext(storage::ReaderClb *clb) override;
-  void readTimePoint(storage::ReaderClb *clb);
 
   bool check_meas(const Meas &m) const;
 
   Reader_ptr clone() const override;
   void reset() override;
 
-  bool is_time_point_reader;
-
   CursorList _cursors;
-  ReadChunkMap _tp_chunks;
   dariadb::Flag _flag;
   dariadb::Time _from;
   dariadb::Time _to;
   dariadb::IdArray _ids;
-  bool _tp_readed;
   bool end;
-  IdArray _not_exist;
 
   typedef std::tuple<dariadb::Id, dariadb::Time> IdTime;
-  std::set<IdTime> _tp_readed_times;
-
   std::mutex _locker, _locker_tp;
 };
 
