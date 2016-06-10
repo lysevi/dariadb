27f4208bc42ef3541965c9cca978224a297d40da
Sergey Lyubimov <lysevi@gmail.com>
2016-06-10 08:05:36 +0300

    warnings.

--- libdariadb/utils/crc.cpp
+++ libdariadb/utils/crc.cpp
@@ -1,8 +1,8 @@
 #include "crc.h"
 #include <boost/crc.hpp>
 
-size_t dariadb::utils::crc32(const void*buffer, const size_t size){
+uint32_t dariadb::utils::crc32(const void*buffer, const size_t size){
     boost::crc_32_type result;
     result.process_bytes(buffer,size);
-    return result.checksum();
+    return static_cast<uint32_t>(result.checksum());
 }
--- libdariadb/utils/crc.h
+++ libdariadb/utils/crc.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 
 namespace dariadb {
 namespace utils {
-size_t crc32(const void *buffer, const size_t size);
+uint32_t crc32(const void *buffer, const size_t size);
 }
 }
