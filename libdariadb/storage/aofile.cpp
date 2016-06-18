a7eb2c7066b3e3f7bf6fc220e753115b31972db7
Sergey Lyubimov <lysevi@gmail.com>
2016-06-18 20:07:10 +0400

    aofile: common test passed.

--- libdariadb/storage/aofile.cpp
+++ libdariadb/storage/aofile.cpp
@@ -1,6 +1,8 @@
 #include "aofile.h"
+#include "../flags.h"
 #include "../utils/fs.h"
 #include "inner_readers.h"
+#include <algorithm>
 #include <cassert>
 #include <cstring>
 #include <cstdio>
@@ -47,18 +49,18 @@ public:
         throw MAKE_EXCEPTION("aof: file open error");
     }
     std::map<dariadb::Id, std::set<Meas, meas_time_compare_less>> sub_result;
-    if(file!=nullptr){
-        while(1){
-            Meas val=Meas::empty();
-            if(fread(&val,sizeof(Meas),size_t(1),file)==0){
-                break;
-            }
-            if(val.inQuery(q.ids,q.flag,q.from,q.to)){
-                 sub_result[val.id].insert(val);
-            }
+
+    while(1){
+        Meas val=Meas::empty();
+        if(fread(&val,sizeof(Meas),size_t(1),file)==0){
+            break;
         }
-        std::fclose(file);
+        if(val.inQuery(q.ids,q.flag,q.from,q.to)){
+            sub_result[val.id].insert(val);
+        }
     }
+    std::fclose(file);
+
     for (auto &kv : sub_result) {
       raw->_ids.push_back(kv.first);
       for (auto &m : kv.second) {
@@ -71,28 +73,160 @@ public:
 
   Reader_ptr readInTimePoint(const QueryTimePoint &q) {
     std::lock_guard<std::mutex> lock(_mutex);
-    return nullptr;
+    dariadb::IdSet readed_ids;
+    dariadb::Meas::Id2Meas sub_res;
+
+    auto file=std::fopen(_params.path.c_str(), "rb");
+    if(file==nullptr){
+        throw MAKE_EXCEPTION("aof: file open error");
+    }
+     while(1){
+        Meas val=Meas::empty();
+        if(fread(&val,sizeof(Meas),size_t(1),file)==0){
+            break;
+        }
+        if (val.inQuery(q.ids, q.flag) && (val.time <= q.time_point)) {
+          replace_if_older(sub_res, val);
+          readed_ids.insert(val.id);
+        }
+    }
+    std::fclose(file);
+
+    if (!q.ids.empty() && readed_ids.size() != q.ids.size()) {
+      for (auto id : q.ids) {
+        if (readed_ids.find(id) == readed_ids.end()) {
+          auto e = Meas::empty(id);
+          e.flag = Flags::_NO_DATA;
+          e.time = q.time_point;
+          sub_res[id] = e;
+        }
+      }
+    }
+
+    TP_Reader *raw = new TP_Reader;
+    for (auto kv : sub_res) {
+      raw->_values.push_back(kv.second);
+      raw->_ids.push_back(kv.first);
+    }
+
+    raw->reset();
+    return Reader_ptr(raw);
+
+  }
+
+  void replace_if_older(dariadb::Meas::Id2Meas &s, const dariadb::Meas &m) const {
+    auto fres = s.find(m.id);
+    if (fres == s.end()) {
+      s.insert(std::make_pair(m.id, m));
+    } else {
+      if (fres->second.time < m.time) {
+        s.insert(std::make_pair(m.id, m));
+      }
+    }
   }
 
   Reader_ptr currentValue(const IdArray &ids, const Flag &flag) {
     std::lock_guard<std::mutex> lock(_mutex);
-    return readInTimePoint(QueryTimePoint(ids, flag, this->maxTime()));
+    dariadb::Meas::Id2Meas sub_res;
+    dariadb::IdSet readed_ids;
+    auto file=std::fopen(_params.path.c_str(), "rb");
+    if(file==nullptr){
+        throw MAKE_EXCEPTION("aof: file open error");
+    }
+     while(1){
+        Meas val=Meas::empty();
+        if(fread(&val,sizeof(Meas),size_t(1),file)==0){
+            break;
+        }
+        replace_if_older(sub_res, val);
+        readed_ids.insert(val.id);
+    }
+    std::fclose(file);
+
+    if (!ids.empty() && readed_ids.size() != ids.size()) {
+      for (auto id : ids) {
+        if (readed_ids.find(id) == readed_ids.end()) {
+          auto e = Meas::empty(id);
+          e.flag = Flags::_NO_DATA;
+          e.time = dariadb::Time(0);
+          sub_res[id] = e;
+        }
+      }
+    }
+
+    TP_Reader *raw = new TP_Reader;
+    for (auto kv : sub_res) {
+      raw->_values.push_back(kv.second);
+      raw->_ids.push_back(kv.first);
+    }
+
+    raw->reset();
+    return Reader_ptr(raw);
+
   }
 
   dariadb::Time minTime() const {
     std::lock_guard<std::mutex> lock(_mutex);
-    return dariadb::MAX_TIME;
+    auto file=std::fopen(_params.path.c_str(), "rb");
+    if(file==nullptr){
+        throw MAKE_EXCEPTION("aof: file open error");
+    }
+
+    dariadb::Time result=dariadb::MAX_TIME;
+
+    while(1){
+        Meas val=Meas::empty();
+        if(fread(&val,sizeof(Meas),size_t(1),file)==0){
+            break;
+        }
+       result=std::min(val.time,result);
+    }
+    std::fclose(file);
+    return result;
   }
 
   dariadb::Time maxTime() const {
-    std::lock_guard<std::mutex> lock(_mutex);
-    return dariadb::MIN_TIME;
+    auto file=std::fopen(_params.path.c_str(), "rb");
+    if(file==nullptr){
+        throw MAKE_EXCEPTION("aof: file open error");
+    }
+
+    dariadb::Time result=dariadb::MIN_TIME;
+
+    while(1){
+        Meas val=Meas::empty();
+        if(fread(&val,sizeof(Meas),size_t(1),file)==0){
+            break;
+        }
+       result=std::max(val.time,result);
+    }
+    std::fclose(file);
+     return result;
   }
 
   bool minMaxTime(dariadb::Id id, dariadb::Time *minResult,
                   dariadb::Time *maxResult) {
-    std::lock_guard<std::mutex> lock(_mutex);
-    return false;
+    auto file=std::fopen(_params.path.c_str(), "rb");
+    if(file==nullptr){
+        throw MAKE_EXCEPTION("aof: file open error");
+    }
+
+    *minResult=dariadb::MAX_TIME;
+    *maxResult=dariadb::MIN_TIME;
+    bool result=false;
+    while(1){
+        Meas val=Meas::empty();
+        if(fread(&val,sizeof(Meas),size_t(1),file)==0){
+            break;
+        }
+       if(val.id==id){
+           result=true;
+           *minResult=std::min(*minResult,val.time);
+           *maxResult=std::max(*maxResult,val.time);
+       }
+    }
+    std::fclose(file);
+    return result;
   }
 
   void flush() {
