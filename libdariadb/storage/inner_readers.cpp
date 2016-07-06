131b0def2f7be284a18d3d16d04413c7542a2ed0
Sergey Lyubimov <lysevi@gmail.com>
2016-07-06 19:17:01 +0400

    rm unused innerReader.

--- libdariadb/storage/inner_readers.cpp
+++ libdariadb/storage/inner_readers.cpp
@@ -6,62 +6,6 @@ using namespace dariadb;
 using namespace dariadb::compression;
 using namespace dariadb::storage;
 
-InnerReader::InnerReader(dariadb::Flag flag, dariadb::Time from, dariadb::Time to)
-    : _values{}, _flag(flag), _from(from), _to(to) {
-  end = false;
-}
-
-bool InnerReader::isEnd() const {
-  return this->end;
-}
-
-dariadb::IdArray InnerReader::getIds() const {
-  return _ids;
-}
-
-void InnerReader::readNext(storage::ReaderClb *clb) {
-  std::lock_guard<std::mutex> lg(_locker);
-
-  for (auto id : this->_ids) {
-    for (auto sub : _values) {
-      if (sub.id == id) {
-        if (check_meas(sub)) {
-          clb->call(sub);
-        }
-      }
-    }
-  }
-  end = true;
-}
-
-bool InnerReader::check_meas(const Meas &m) const {
-  using utils::inInterval;
-
-  if (m.inFlag(_flag) && (m.inInterval(_from, _to))) {
-    return true;
-  }
-  return false;
-}
-
-Reader_ptr InnerReader::clone() const {
-  auto res = std::make_shared<InnerReader>(_flag, _from, _to);
-  res->_values = _values;
-  res->_flag = _flag;
-  res->_from = _from;
-  res->_to = _to;
-  res->_ids = _ids;
-  res->end = end;
-  res->reset();
-  return res;
-}
-void InnerReader::reset() {
-  end = false;
-}
-
-size_t InnerReader::size() {
-  return _values.size();
-}
-
 TP_Reader::TP_Reader() {
   _values_iterator = this->_values.end();
 }
--- libdariadb/storage/inner_readers.h
+++ libdariadb/storage/inner_readers.h
@@ -9,32 +9,6 @@
 namespace dariadb {
 namespace storage {
 
-typedef std::map<Id, dariadb::storage::ChunksList> ReadChunkMap;
-
-class InnerReader : public Reader {
-public:
-  InnerReader(dariadb::Flag flag, dariadb::Time from, dariadb::Time to);
-
-  bool isEnd() const override;
-
-  dariadb::IdArray getIds() const override;
-  void readNext(storage::ReaderClb *clb) override;
-
-  bool check_meas(const Meas &m) const;
-
-  Reader_ptr clone() const override;
-  void reset() override;
-  size_t size() override;
-  Meas::MeasList _values;
-  dariadb::Flag _flag;
-  dariadb::Time _from;
-  dariadb::Time _to;
-  dariadb::IdArray _ids;
-  bool end;
-
-  std::mutex _locker, _locker_tp;
-};
-
 class TP_Reader : public storage::Reader {
 public:
   TP_Reader();
