507e9b75f573f4d37ffcba1ac0403cbf717e1e78
Sergey Lyubimov <lysevi@gmail.com>
2016-07-19 08:13:52 +0300

    PageIndex: rm PageLinkCursor.

--- libdariadb/storage/index.cpp
+++ libdariadb/storage/index.cpp
@@ -9,113 +9,26 @@
 #include <boost/thread/shared_mutex.hpp>
 
 using namespace dariadb::storage;
+using dariadb::utils::inInterval;
 
-class PageLinksCursor {
-public:
-  PageLinksCursor(PageIndex *page, const dariadb::IdArray &ids, dariadb::Time from,
-                  dariadb::Time to, dariadb::Flag flag)
-      : link(page), _ids(ids), _from(from), _to(to), _flag(flag) {}
+inline bool check_index_rec(IndexReccord &it, dariadb::Time from, dariadb::Time to) {
+  return (inInterval(from, to, it.minTime) || inInterval(from, to, it.maxTime)) ||
+         (inInterval(it.minTime, it.maxTime, from) ||
+          inInterval(it.minTime, it.maxTime, to));
+}
 
-  ~PageLinksCursor() {
-    if (link != nullptr) {
-      link = nullptr;
-    }
-  }
-
-  bool is_end() const { return _is_end; }
-
-  void readNext() {
-    if (read_poses.empty()) {
-      _is_end = true;
-      return;
-    }
-    auto current_pos = read_poses.front();
-    auto _index_it = this->link->index[read_poses.front()];
-    read_poses.pop_front();
-    for (; !_is_end;) {
-      if (_is_end) {
-        _is_end = true;
-        break;
-      }
-
-      ChunkLink sub_result;
-      sub_result.id = _index_it.chunk_id;
-      sub_result.pos = current_pos;
-      sub_result.maxTime = _index_it.maxTime;
-      sub_result.id_bloom = _index_it.id_bloom;
-      this->resulted_links.push_back(sub_result);
-      break;
-    }
-    if (read_poses.empty()) {
-      _is_end = true;
-      return;
-    }
-  }
-
-  bool check_index_rec(IndexReccord &it) const {
-    return ((dariadb::utils::inInterval(_from, _to, it.minTime)) ||
-            (dariadb::utils::inInterval(_from, _to, it.maxTime))) ||
-           (dariadb::utils::inInterval(it.minTime, it.maxTime, _from) ||
-            dariadb::utils::inInterval(it.minTime, it.maxTime, _to));
-  }
-
-  void reset_pos() { // start read from begining;
-    _is_end = false;
-    this->read_poses.clear();
-
-    for (uint32_t pos = 0; pos < this->link->iheader->count; ++pos) {
-
-      auto _index_it = link->index[pos];
-      if (!_index_it.is_init || !_index_it.commit) {
-        continue;
-      }
-      if (dariadb::utils::inInterval(_index_it.minTime, _index_it.maxTime, _from) ||
-          dariadb::utils::inInterval(_index_it.minTime, _index_it.maxTime, _to) ||
-          dariadb::utils::inInterval(_from, _to, _index_it.minTime) ||
-          dariadb::utils::inInterval(_from, _to, _index_it.maxTime)) {
-        bool bloom_result = false;
-        for (auto i : _ids) {
-          bloom_result = check_blooms(_index_it, i);
-          if (bloom_result) {
-            break;
-          }
-        }
-        if (bloom_result) {
-          if (check_index_rec(_index_it)) {
-            this->read_poses.push_back(pos);
-            assert(this->read_poses.size() <= this->link->iheader->count);
-          }
-        }
-      }
-    }
-    if (read_poses.empty()) {
-      _is_end = true;
-    }
-  }
-
-  bool check_blooms(const IndexReccord &_index_it, dariadb::Id id) const {
-    auto id_bloom_result = false;
-    if (dariadb::storage::bloom_check(_index_it.id_bloom, id)) {
-      id_bloom_result = true;
-    }
-    auto flag_bloom_result = false;
-    if (_flag == dariadb::Flag(0) ||
-        dariadb::storage::bloom_check(_index_it.flag_bloom, _flag)) {
-      flag_bloom_result = true;
-    }
-    return id_bloom_result && flag_bloom_result;
-  }
-
-  ChunkLinkList resulted_links;
-
-protected:
-  PageIndex *link;
-  bool _is_end;
-  dariadb::IdArray _ids;
-  dariadb::Time _from, _to;
-  dariadb::Flag _flag;
-  std::list<uint32_t> read_poses;
-};
+inline bool check_blooms(const IndexReccord &_index_it, dariadb::Id id, dariadb::Flag flag) {
+	auto id_bloom_result = false;
+	if (dariadb::storage::bloom_check(_index_it.id_bloom, id)) {
+		id_bloom_result = true;
+	}
+	auto flag_bloom_result = false;
+	if (flag == dariadb::Flag(0) ||
+		dariadb::storage::bloom_check(_index_it.flag_bloom, flag)) {
+		flag_bloom_result = true;
+	}
+	return id_bloom_result && flag_bloom_result;
+}
 
 PageIndex::~PageIndex() {
   iheader->is_closed = true;
@@ -161,15 +74,33 @@ PageIndex_ptr PageIndex::open(const std::string &filename, bool read_only) {
 ChunkLinkList PageIndex::get_chunks_links(const dariadb::IdArray &ids, dariadb::Time from,
                                           dariadb::Time to, dariadb::Flag flag) {
   boost::shared_lock<boost::shared_mutex> lg(_locker);
+  ChunkLinkList result;
+  for (uint32_t pos = 0; pos < this->iheader->count; ++pos) {
 
-  PageLinksCursor c(this, ids, from, to, flag);
-  c.reset_pos();
-
-  while (!c.is_end()) {
-    c.readNext();
+    auto _index_it = this->index[pos];
+    if (!_index_it.is_init || !_index_it.commit) {
+      continue;
+    }
+    if (check_index_rec(_index_it, from, to)) {
+      bool bloom_result = false;
+      for (auto i : ids) {
+        bloom_result = check_blooms(_index_it, i, flag);
+        if (bloom_result) {
+          break;
+        }
+      }
+      if (bloom_result) {
+        ChunkLink sub_result;
+        sub_result.id = _index_it.chunk_id;
+        sub_result.pos = pos;
+        sub_result.maxTime = _index_it.maxTime;
+        sub_result.id_bloom = _index_it.id_bloom;
+        result.push_back(sub_result);
+      }
+    }
   }
 
-  return c.resulted_links;
+  return result;
 }
 
 IndexHeader PageIndex::readIndexHeader(std::string ifile) {
