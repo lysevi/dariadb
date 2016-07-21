9e68d68b3675b0da66cd012d8e6e5b54907ba1f3
Sergey Lyubimov <lysevi@gmail.com>
2016-07-21 10:57:13 +0300

    libcommon_test: refact.

--- tests/test_common.h
+++ tests/test_common.h
@@ -6,7 +6,6 @@
 #include <string>
 
 namespace dariadb_test {
-const size_t sizeInMb10 = 10 * 1024 * 1024;
 const size_t copies_count = 100;
 
 void checkAll(dariadb::Meas::MeasList res, std::string msg, dariadb::Time from,
@@ -15,5 +14,5 @@ void checkAll(dariadb::Meas::MeasList res, std::string msg, dariadb::Time from,
 void storage_test_check(dariadb::storage::MeasStorage *as, dariadb::Time from,
                         dariadb::Time to, dariadb::Time step);
 
-void readIntervalCommonTest(dariadb::storage::MeasStorage *ds);
+/*void readIntervalCommonTest(dariadb::storage::MeasStorage *ds);*/
 }
