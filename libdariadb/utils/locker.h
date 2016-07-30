69ae73b27a5fa8ada089644a610c6613f31e94fa
Sergey Lyubimov <lysevi@gmail.com>
2016-07-30 07:30:52 +0300

    utils::Locker: yield thread when try-lock is false.

--- libdariadb/utils/locker.h
+++ libdariadb/utils/locker.h
@@ -1,20 +1,25 @@
 #pragma once
 
 #include <atomic>
-#include <memory>
 #include <mutex> //for lock_guard
+#include <thread>
 
 namespace dariadb {
 namespace utils {
 // using Locker=std::mutex;
-
+const size_t LOCKER_MAX_TRY=10;
 class Locker {
   std::atomic_flag locked = ATOMIC_FLAG_INIT;
 
 public:
   void lock() {
+      size_t num_try=0;
     while (locked.test_and_set(std::memory_order_acquire)) {
-      ;
+      num_try++;
+      if(num_try>=LOCKER_MAX_TRY){
+          num_try=0;
+          std::this_thread::yield();
+      }
     }
   }
   void unlock() { locked.clear(std::memory_order_release); }
