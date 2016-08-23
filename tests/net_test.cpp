e2c390e201003fe01f0d7015ae217cb766a3fda2
Sergey Lyubimov <lysevi@gmail.com>
2016-08-23 16:14:33 +0400

    lock on test

--- tests/net_test.cpp
+++ tests/net_test.cpp
@@ -3,11 +3,14 @@
 #include <boost/test/unit_test.hpp>
 #include <net/client.h>
 #include <net/server.h>
+#include <utils/locker.h>
 
 const dariadb::net::Server::Param server_param(2001);
 const dariadb::net::Client::Param client_param("127.0.0.1", 2001);
 
 BOOST_AUTO_TEST_CASE(Instance) {
+    dariadb::utils::Locker lock;
+    std::lock_guard<dariadb::utils::Locker> lg(lock);
   dariadb::net::Server s(server_param);
   BOOST_CHECK_EQUAL(s.connections_accepted(),size_t(0));
 
