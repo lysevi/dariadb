a779fe431694c7503bf36a3358918763330bb123
Sergey Lyubimov <lysevi@gmail.com>
2016-08-23 16:56:22 +0300

    net: server async begin.

--- libdariadb/net/server.cpp
+++ libdariadb/net/server.cpp
@@ -7,6 +7,8 @@
 #include <functional>
 #include <thread>
 #include <sstream>
+#include <istream>
+#include <list>
 
 using namespace std::placeholders;
 using namespace boost::asio;
@@ -18,94 +20,134 @@ using namespace dariadb::net;
 typedef boost::shared_ptr<ip::tcp::socket> socket_ptr;
 typedef boost::shared_ptr<ip::tcp::acceptor> acceptor_ptr;
 
-struct ClientIO{
-    socket_ptr sock;
-    streambuf buff;
-
-    ClientIO(socket_ptr _sock){
-        sock=_sock;
-    }
-
-    void readHello(){
-
-    }
-};
-
 class Server::Private {
 public:
-  Private(const Server::Param &p):_params(p),_stop_flag(false),_is_runned_flag(false){
-      _connections_accepted.store(0);
-      _thread_handler=std::thread(&Server::Private::server_thread, this);
-  }
 
-  ~Private(){
-      stop();
-  }
+    struct ClientIO{
+        socket_ptr sock;
+        streambuf buff;
+        std::string host;
 
-  void stop(){
-      _stop_flag=true;
-      _service.stop();
-      _thread_handler.join();
-  }
-  void server_thread(){
-      logger_info("server: start server on ", _params.port, "...");
+        Server::Private *srv;
+
+        ClientIO(socket_ptr _sock, Server::Private *_srv){
+            sock=_sock;
+            srv=_srv;
+        }
+
+        void readHello(){
+            logger("server: read hello...");
+            async_read_until(*sock.get(),buff,'\n',std::bind(&ClientIO::onHello,this,_1,_2));
+        }
+
+        void onHello(const boost::system::error_code & err, size_t read_bytes){
+            logger("server: on hello...");
+            if(err){
+                THROW_EXCEPTION_SS("server: ClienIO::onHello "<<err.message());
+            }
+            {
+                std::istream iss(&this->buff);
+                std::string msg;
+                std::getline(iss,msg);
+                logger("server: clientio::onHello - ",msg);
+            }
+
+            if(read_bytes<HELLO_PREFIX.size()){
+                boost::asio::streambuf::const_buffers_type bfs = buff.data();
+                std::string recv_data{buffers_begin(bfs),buffers_end(bfs)};
+                logger_fatal("server: bad hello - ", recv_data);
+            }else{
+                std::istream iss(&this->buff);
+                std::string readed_str;
+                iss >> readed_str;
+                if(readed_str!=HELLO_PREFIX){
+                    THROW_EXCEPTION_SS("server: bad hello prefix "<<readed_str);
+                }
+                iss >> readed_str;
+                host=readed_str;
+                this->srv->client_accept(this);
+            }
+        }
+    };
+
+    typedef std::shared_ptr<ClientIO> ClientIO_ptr;
 
 
-      ip::tcp::endpoint ep(ip::tcp::v4(), _params.port);
-      _acc = acceptor_ptr{new ip::tcp::acceptor(_service, ep)};
-      socket_ptr sock(new ip::tcp::socket(_service));
-      start_accept(sock);
-      logger_info("server: OK");
-      while(!_stop_flag){
-          _service.poll_one();
-          _is_runned_flag.store(true);
-      }
 
-  }
-
-  void start_accept(socket_ptr sock) {
-    _acc->async_accept(*sock, std::bind(&Server::Private::handle_accept, this, sock, _1));
-  }
-
-  void handle_accept(socket_ptr sock, const boost::system::error_code &err) {
-      if(err){
-          THROW_EXCEPTION_SS("dariadb::server: error on accept - "<<err.message());
-      }
-
-    logger_info("server: accept connection.");
-
-    char buff[1024];
-    auto readed=sock->read_some(buffer(buff,1024));
-    if(readed<HELLO_PREFIX.size()){
-        logger_fatal("server: bad hello - ", buff);
-    }else{
-        std::istringstream iss(buff);
-        std::string readed_str;
-        iss >> readed_str;
-        assert(readed_str==HELLO_PREFIX);
-        iss >> readed_str;
-        logger_info("server: hello from {", readed_str,'}');
-        _connections_accepted += 1;
+    Private(const Server::Param &p):_params(p),_stop_flag(false),_is_runned_flag(false){
+        _connections_accepted.store(0);
+        _thread_handler=std::thread(&Server::Private::server_thread, this);
     }
-    socket_ptr new_sock(new ip::tcp::socket(_service));
-    start_accept(new_sock);
-  }
 
-  size_t connections_accepted() const { return _connections_accepted.load(); }
+    ~Private(){
+        stop();
+    }
 
-  bool is_runned(){
-      return _is_runned_flag.load();
-  }
+    void stop(){
+        _stop_flag=true;
+        _service.stop();
+        _thread_handler.join();
+    }
+    void server_thread(){
+        logger_info("server: start server on ", _params.port, "...");
 
-  io_service _service;
-  acceptor_ptr _acc;
 
-  std::atomic_size_t _connections_accepted;
-  Server::Param _params;
-  std::thread _thread_handler;
+        ip::tcp::endpoint ep(ip::tcp::v4(), _params.port);
+        _acc = acceptor_ptr{new ip::tcp::acceptor(_service, ep)};
+        socket_ptr sock(new ip::tcp::socket(_service));
+        start_accept(sock);
+        logger_info("server: OK");
+        while(!_stop_flag){
+            _service.poll_one();
+            _is_runned_flag.store(true);
+        }
+        _is_runned_flag.store(false);
+    }
 
-  std::atomic_bool _stop_flag;
-  std::atomic_bool _is_runned_flag;
+    void start_accept(socket_ptr sock) {
+        _acc->async_accept(*sock, std::bind(&Server::Private::handle_accept, this, sock, _1));
+    }
+
+    void handle_accept(socket_ptr sock, const boost::system::error_code &err) {
+        if(err){
+            THROW_EXCEPTION_SS("dariadb::server: error on accept - "<<err.message());
+        }
+
+        logger_info("server: accept connection.");
+
+        ClientIO_ptr new_client{new ClientIO(sock,this)};
+        new_client->readHello();
+
+        socket_ptr new_sock(new ip::tcp::socket(_service));
+        start_accept(new_sock);
+    }
+
+    size_t connections_accepted() const { return _connections_accepted.load(); }
+
+    bool is_runned(){
+        return _is_runned_flag.load();
+    }
+
+    void client_accept(ClientIO*client){
+        _connections_accepted+=1;
+        logger_info("server: hello from {", client->host,'}');
+    }
+
+    void client_disconnect(ClientIO*client){
+
+    }
+
+    io_service _service;
+    acceptor_ptr _acc;
+
+    std::atomic_size_t _connections_accepted;
+    Server::Param _params;
+    std::thread _thread_handler;
+
+    std::atomic_bool _stop_flag;
+    std::atomic_bool _is_runned_flag;
+    //TODO use hashset
+    std::list<ClientIO_ptr> _clients;
 };
 
 Server::Server(const Param &p) : _Impl(new Server::Private(p)) {}
@@ -113,11 +155,11 @@ Server::Server(const Param &p) : _Impl(new Server::Private(p)) {}
 Server::~Server() {}
 
 bool Server::is_runned(){
-  return _Impl->is_runned();
+    return _Impl->is_runned();
 }
 
 size_t Server::connections_accepted() const {
-  return _Impl->connections_accepted();
+    return _Impl->connections_accepted();
 }
 
 void Server::stop(){
