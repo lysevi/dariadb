a7f9c83bcd8315920c51dfaa2d8101463482f8b9
Sergey Lyubimov <lysevi@gmail.com>
2016-10-05 13:56:48 +0300

    lockfree_array - insertion and load.

--- libdariadb/ads/lockfree_array.h
+++ libdariadb/ads/lockfree_array.h
@@ -16,20 +16,20 @@ template <typename T> class LockFreeArray {
 
 public:
   LockFreeArray() {
-    _pos = 0;
+	  _pos.store(0);
     _array = nullptr;
     _size = 0;
   }
 
   LockFreeArray(const size_t size) : _size(size) {
-    _pos = 0;
+    _pos.store(0);
     _array = new std::atomic<T>[_size];
-	std::fill(_array, _array + _size, T());
+    std::fill(_array, _array + _size, T());
   }
 
   LockFreeArray(const LockFreeArray &other) = delete;
-  LockFreeArray(const LockFreeArray &&other)
-      : _size(other._size), _pos(other._pos), _array(other._array) {
+  LockFreeArray(LockFreeArray &&other)
+      : _size(other._size), _pos(other._pos.load()), _array(other._array) {
     other._size = 0;
     other._array = nullptr;
   }
@@ -39,19 +39,39 @@ public:
     }
   }
 
-  size_t cap() const { return _size - _pos; }
   size_t size() const { return _size; }
 
-  bool try_store(size_t index, const T &t) {
+  void store(size_t index, const T &t) {
     assert(index < _size);
-    auto old = _array[index].load();
-    return _array[index].compare_exchange_weak(old, t);
+	T old;
+    do {
+      old = _array[index].load();
+	} while (!_array[index].compare_exchange_weak(old, t));
+  }
+  
+  T operator[](size_t index)const {
+	  return _array[index].load();
   }
 
+  bool insert(const T&t) {
+	  size_t oldPos;
+	  size_t newPos;
+	  do {
+		  oldPos = _pos.load();
+		  newPos=oldPos+1;
+		  if (oldPos >= _size) {
+			  return false;
+		  }
+	  } while (!_pos.compare_exchange_weak(oldPos, newPos));
+	  _array[oldPos].store(t);
+	  return true;
+  }
+
+  size_t cap()const { return _size - _pos.load(); }
 protected:
   size_t _size;
+  std::atomic_size_t _pos;
   std::atomic<T> *_array;
-  size_t _pos;
 };
 }
 }
--- tests/memstorage_test.cpp
+++ tests/memstorage_test.cpp
@@ -14,6 +14,36 @@ BOOST_AUTO_TEST_CASE(TypeTraitArrayLockFreeTest) {
 	BOOST_CHECK_EQUAL(lfs.size(), size_t(10));
 }
 
+
+BOOST_AUTO_TEST_CASE(ArrayLockFreeTest) {
+	{
+		dariadb::utils::LockFreeArray<bool> lf(3);
+		BOOST_CHECK_EQUAL(lf.size(), size_t(3));
+
+		lf.store(0, true);
+		lf.store(1, true);
+
+		dariadb::utils::LockFreeArray<bool> lf_c(std::move(lf));
+		BOOST_CHECK(lf_c[0]);
+		BOOST_CHECK(lf_c[1]);
+		BOOST_CHECK(!lf_c[2]);
+	}
+	{
+		dariadb::utils::LockFreeArray<bool> lf(5);
+		BOOST_CHECK_EQUAL(lf.cap(), lf.size());
+		
+		size_t i = 0;
+		while (lf.insert(true)) {
+			i++;
+		}
+		BOOST_CHECK_EQUAL(lf.size(), i);
+		BOOST_CHECK_EQUAL(lf.cap(), size_t(0));
+		for (size_t i = 0; i < lf.size(); ++i) {
+			BOOST_CHECK(lf[i]);
+		}
+	}
+}
+
 BOOST_AUTO_TEST_CASE(TypeTraitsForMeasurement) {
 	dariadb::utils::RadixPlusTree<dariadb::Time, dariadb::Meas> tree;
 	BOOST_CHECK_EQUAL(tree.keys_count(), size_t(0));
