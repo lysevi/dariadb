abfda24953cd5fbdb407dd07e654f402dc30da90
Sergey Lyubimov <lysevi@gmail.com>
2016-12-07 16:18:15 +0300

    MemChunkAllocator is lockfree now.

--- libdariadb/storage/allocators.cpp
+++ libdariadb/storage/allocators.cpp
@@ -1,25 +1,29 @@
 #include <libdariadb/storage/allocators.h>
-#include <memory>
 #include <cstring>
+#include <memory>
 
 using namespace dariadb;
 using namespace dariadb::storage;
 
-MemChunkAllocator::MemChunkAllocator(size_t maxSize, size_t bufferSize) {
+MemChunkAllocator::MemChunkAllocator(size_t maxSize, size_t bufferSize)
+    : _one_chunk_size(sizeof(ChunkHeader) + bufferSize),
+      _capacity((int)(float(maxSize) / _one_chunk_size)), _free_list(_capacity) {
   _maxSize = maxSize;
   _chunkSize = bufferSize;
   _allocated = size_t(0);
-  size_t one_chunk_size = sizeof(ChunkHeader) + bufferSize;
-  _capacity = (int)(float(_maxSize) / one_chunk_size);
-  size_t buffers_size=_capacity * (_chunkSize+sizeof(ChunkHeader));
 
-  _region=new uint8_t[buffers_size];
-  _headers=reinterpret_cast<ChunkHeader*>(_region);
-  _buffers = _region+_capacity * sizeof(ChunkHeader);
+  size_t buffers_size = _capacity * (_chunkSize + sizeof(ChunkHeader));
 
-  memset(_region,0,buffers_size);
-  for(size_t i=0;i<_capacity;++i){
-      _free_list.push_back(i);
+  _region = new uint8_t[buffers_size];
+  _headers = reinterpret_cast<ChunkHeader *>(_region);
+  _buffers = _region + _capacity * sizeof(ChunkHeader);
+
+  memset(_region, 0, buffers_size);
+  for (size_t i = 0; i < _capacity; ++i) {
+    auto res = _free_list.push(i);
+    if (!res) {
+      THROW_EXCEPTION("engine: MemChunkAllocator::ctor - bad capacity.");
+    }
   }
 }
 
@@ -28,26 +32,24 @@ MemChunkAllocator::~MemChunkAllocator() {
 }
 
 MemChunkAllocator::AllocatedData MemChunkAllocator::allocate() {
-  _locker.lock();
-  if(_free_list.empty()){
-      _locker.unlock();
-      return EMPTY;
+  size_t pos;
+  if (!_free_list.pop(pos)) {
+    return EMPTY;
   }
-  auto pos=_free_list.front();
-  _free_list.pop_front();
   _allocated++;
-  _locker.unlock();
   return AllocatedData(&_headers[pos], &_buffers[pos * _chunkSize], pos);
 }
 
 void MemChunkAllocator::free(const MemChunkAllocator::AllocatedData &d) {
-	auto header = d.header;
-	auto buffer = d.buffer;
-	auto position = d.position;
+  auto header = d.header;
+  auto buffer = d.buffer;
+  auto position = d.position;
   memset(header, 0, sizeof(ChunkHeader));
   memset(buffer, 0, _chunkSize);
-  _locker.lock();
+
   _allocated--;
-  _free_list.push_back(position);
-  _locker.unlock();
+  auto res = _free_list.push(position);
+  if (!res) {
+    THROW_EXCEPTION("engine: MemChunkAllocator::free - bad capacity.");
+  }
 }
--- libdariadb/storage/allocators.h
+++ libdariadb/storage/allocators.h
@@ -1,47 +1,50 @@
 #pragma once
 
 #include <libdariadb/st_exports.h>
-#include <libdariadb/utils/locker.h>
 #include <libdariadb/storage/chunk.h>
+#include <libdariadb/utils/locker.h>
 #include <libdariadb/utils/utils.h>
 #include <memory>
+
+#include <boost/lockfree/queue.hpp>
 
 namespace dariadb {
 namespace storage {
 
-struct MemChunkAllocator:public utils::NonCopy {
-	struct AllocatedData {
-		ChunkHeader *header;
-		uint8_t *buffer;
-		size_t position;
-		AllocatedData(ChunkHeader *h, uint8_t *buf, size_t pos) {
-			header = h;
-			buffer=buf;
-			position = pos;
-		}
-		AllocatedData() {
-			header = nullptr;
-			buffer = nullptr;
-			position = std::numeric_limits<size_t>::max();
-		}
-	};
-  
-  const AllocatedData EMPTY = AllocatedData(nullptr, nullptr, std::numeric_limits<size_t>::max());
+struct MemChunkAllocator : public utils::NonCopy {
+  struct AllocatedData {
+    ChunkHeader *header;
+    uint8_t *buffer;
+    size_t position;
+    AllocatedData(ChunkHeader *h, uint8_t *buf, size_t pos) {
+      header = h;
+      buffer = buf;
+      position = pos;
+    }
+    AllocatedData() {
+      header = nullptr;
+      buffer = nullptr;
+      position = std::numeric_limits<size_t>::max();
+    }
+  };
 
-  size_t _maxSize;        /// max size in bytes)
-  size_t _chunkSize;      /// size of chunk
-  size_t _capacity;       /// max size in chunks
-  size_t _allocated;      /// already allocated count of chunks.
+  const AllocatedData EMPTY =
+      AllocatedData(nullptr, nullptr, std::numeric_limits<size_t>::max());
+
+  size_t _one_chunk_size;
+  size_t _maxSize;   /// max size in bytes)
+  size_t _chunkSize; /// size of chunk
+  size_t _capacity;  /// max size in chunks
+  size_t _allocated; /// already allocated count of chunks.
 
   ChunkHeader *_headers;
   uint8_t *_buffers;
   uint8_t *_region;
 
-  std::list<size_t> _free_list;
-  utils::Locker _locker;
+  boost::lockfree::queue<size_t> _free_list;
 
   EXPORT MemChunkAllocator(size_t maxSize, size_t bufferSize);
-  MemChunkAllocator(const MemChunkAllocator&)=delete;
+  MemChunkAllocator(const MemChunkAllocator &) = delete;
   EXPORT ~MemChunkAllocator();
   EXPORT AllocatedData allocate();
   EXPORT void free(const AllocatedData &d);
