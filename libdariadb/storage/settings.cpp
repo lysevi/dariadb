9538bcab515db56becad5918f098132495f6f581
Sergey Lyubimov <lysevi@gmail.com>
2016-12-28 07:50:07 +0300

    settings refact.

--- libdariadb/storage/settings.cpp
+++ libdariadb/storage/settings.cpp
@@ -1,11 +1,11 @@
-#include <extern/json/src/json.hpp>
-#include <fstream>
 #include <libdariadb/meas.h>
 #include <libdariadb/storage/settings.h>
 #include <libdariadb/utils/exception.h>
 #include <libdariadb/utils/fs.h>
 #include <libdariadb/utils/logger.h>
 #include <libdariadb/utils/strings.h>
+#include <extern/json/src/json.hpp>
+#include <fstream>
 
 using namespace dariadb::storage;
 using json = nlohmann::json;
@@ -13,60 +13,58 @@ using json = nlohmann::json;
 const uint64_t AOF_BUFFER_SIZE = 2000;
 const uint64_t AOF_FILE_SIZE = sizeof(dariadb::Meas) * AOF_BUFFER_SIZE * 4;
 const uint32_t CHUNK_SIZE = 1024;
-const size_t MAXIMUM_MEMORY_LIMIT = 100 * 1024 * 1024; //100 mb
+const size_t MAXIMUM_MEMORY_LIMIT = 100 * 1024 * 1024; // 100 mb
 
-const std::string c_aof_max_size="aof_max_size";
-const std::string c_aof_buffer_size="aof_buffer_size";
-const std::string c_chunk_size="chunk_size";
-const std::string c_strategy="strategy";
-const std::string c_memory_limit="memory_limit";
-const std::string c_percent_when_start_droping="percent_when_start_droping";
-const std::string c_percent_to_drop="percent_to_drop";
+const std::string c_aof_max_size = "aof_max_size";
+const std::string c_aof_buffer_size = "aof_buffer_size";
+const std::string c_chunk_size = "chunk_size";
+const std::string c_strategy = "strategy";
+const std::string c_memory_limit = "memory_limit";
+const std::string c_percent_when_start_droping = "percent_when_start_droping";
+const std::string c_percent_to_drop = "percent_to_drop";
 
 std::string settings_file_path(const std::string &path) {
   return dariadb::utils::fs::append_path(path, SETTINGS_FILE_NAME);
 }
 
 #ifndef MSVC
-template<> std::string Settings::Option<STRATEGY>::value_str()const {
-	std::stringstream ss;
-	ss << this->value();
-	return ss.str();
+template <> std::string Settings::ReadOnlyOption<STRATEGY>::value_str() const {
+  std::stringstream ss;
+  ss << this->value();
+  return ss.str();
 }
-template<> std::string Settings::Option<std::string>::value_str()const {
-	return this->value();
+template <> std::string Settings::ReadOnlyOption<std::string>::value_str() const {
+  return this->value();
 }
 #endif
 
-BaseOption::~BaseOption() {
-}
+BaseOption::~BaseOption() {}
 
-
-
-Settings::Settings(const std::string&path_to_storage):
-	storage_path(nullptr, "storage path",path_to_storage),
-	raw_path(nullptr, "raw path", utils::fs::append_path(path_to_storage,"raw")),
-	bystep_path(nullptr, "bystep path", utils::fs::append_path(path_to_storage, "bystep")),
-	aof_max_size(this, c_aof_max_size, AOF_FILE_SIZE),
-	aof_buffer_size(this, c_aof_buffer_size, AOF_BUFFER_SIZE),
-	chunk_size(this,c_chunk_size, CHUNK_SIZE),
-	strategy(this, c_strategy, STRATEGY::COMPRESSED),
-	memory_limit(this, c_memory_limit, MAXIMUM_MEMORY_LIMIT),
-	percent_when_start_droping(this, c_percent_when_start_droping, float(0.75)),
-	percent_to_drop(this, c_percent_to_drop, float(0.1)){
+Settings::Settings(const std::string &path_to_storage)
+    : storage_path(nullptr, "storage path", path_to_storage),
+      raw_path(nullptr, "raw path", utils::fs::append_path(path_to_storage, "raw")),
+      bystep_path(nullptr, "bystep path",
+                  utils::fs::append_path(path_to_storage, "bystep")),
+      aof_max_size(this, c_aof_max_size, AOF_FILE_SIZE),
+      aof_buffer_size(this, c_aof_buffer_size, AOF_BUFFER_SIZE),
+      chunk_size(this, c_chunk_size, CHUNK_SIZE),
+      strategy(this, c_strategy, STRATEGY::COMPRESSED),
+      memory_limit(this, c_memory_limit, MAXIMUM_MEMORY_LIMIT),
+      percent_when_start_droping(this, c_percent_when_start_droping, float(0.75)),
+      percent_to_drop(this, c_percent_to_drop, float(0.1)) {
   auto f = settings_file_path(storage_path.value());
   if (utils::fs::path_exists(f)) {
     load(f);
   } else {
-	dariadb::utils::fs::mkdir(storage_path.value());
-	dariadb::utils::fs::mkdir(raw_path.value());
-	dariadb::utils::fs::mkdir(bystep_path.value());
+    dariadb::utils::fs::mkdir(storage_path.value());
+    dariadb::utils::fs::mkdir(raw_path.value());
+    dariadb::utils::fs::mkdir(bystep_path.value());
     save();
   }
   load_min_max = true;
 }
 
-Settings::~Settings(){}
+Settings::~Settings() {}
 
 void Settings::set_default() {
   logger("engine: Settings set default Settings");
@@ -95,10 +93,10 @@ void Settings::save(const std::string &file) {
   logger("engine: Settings save to ", file);
   json js;
 
-  for (auto&o : _all_options) {
-	  js[o.first] = o.second->value_str();
+  for (auto &o : _all_options) {
+    js[o.first] = o.second->value_str();
   }
-  
+
   std::fstream fs;
   fs.open(file, std::ios::out);
   if (!fs.is_open()) {
@@ -113,32 +111,31 @@ void Settings::load(const std::string &file) {
   logger("engine: Settings loading ", file);
   std::string content = dariadb::utils::fs::read_file(file);
   json js = json::parse(content);
-  for (auto&o : _all_options) {
-	  auto str_val = js[o.first];
-	  o.second->from_string(str_val);
+  for (auto &o : _all_options) {
+    auto str_val = js[o.first];
+    o.second->from_string(str_val);
   }
 }
 
-std::string Settings::dump(){
-   auto content=dariadb::utils::fs::read_file(settings_file_path(storage_path.value()));
-   json js = json::parse(content);
-   std::stringstream ss;
-   ss<<js.dump(1)<<std::endl;
-   return ss.str();
+std::string Settings::dump() {
+  auto content = dariadb::utils::fs::read_file(settings_file_path(storage_path.value()));
+  json js = json::parse(content);
+  std::stringstream ss;
+  ss << js.dump(1) << std::endl;
+  return ss.str();
 }
 
-void Settings::change(std::string& expression){
-    auto splited=utils::strings::split(expression,'=');
-    if(splited.size()!=2){
-        THROW_EXCEPTION("bad format. use: name=value");
-    }
+void Settings::change(std::string &expression) {
+  auto splited = utils::strings::split(expression, '=');
+  if (splited.size() != 2) {
+    THROW_EXCEPTION("bad format. use: name=value");
+  }
 
-	auto fres = _all_options.find(splited[0]);
-	if (fres != _all_options.end()) {
-		logger_info("engine: change ", fres->first);
-		fres->second->from_string(splited[1]);
-	}
-	else {
-		logger_fatal("engine: engine: bad expression ", expression);
-	}
+  auto fres = _all_options.find(splited[0]);
+  if (fres != _all_options.end()) {
+    logger_info("engine: change ", fres->first);
+    fres->second->from_string(splited[1]);
+  } else {
+    logger_fatal("engine: engine: bad expression ", expression);
+  }
 }
--- libdariadb/storage/settings.h
+++ libdariadb/storage/settings.h
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <libdariadb/meas.h>
-#include <libdariadb/utils/logger.h>
-#include <libdariadb/utils/async/thread_pool.h>
-#include <libdariadb/storage/strategy.h>
 #include <libdariadb/st_exports.h>
+#include <libdariadb/storage/strategy.h>
+#include <libdariadb/utils/async/thread_pool.h>
+#include <libdariadb/utils/logger.h>
 
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 
 namespace dariadb {
 namespace storage {
@@ -15,53 +15,66 @@ namespace storage {
 const std::string SETTINGS_FILE_NAME = "Settings";
 class BaseOption {
 public:
-	EXPORT virtual ~BaseOption();
-	virtual std::string key_str()const =0;
-	virtual std::string value_str()const =0;
-	virtual void from_string(const std::string&s)=0;
+  EXPORT virtual ~BaseOption();
+  /// return key in option file.
+  virtual std::string key_str() const = 0;
+  /// convert option value to string
+  virtual std::string value_str() const = 0;
+  /// read from string :)
+  virtual void from_string(const std::string &s) = 0;
 };
 
+/**
+settings - is a dictionary on disk.
+*/
 class Settings {
   std::unordered_map<std::string, BaseOption *> _all_options;
-
-  template <typename T> class Option : public BaseOption {
+  // TODO make non template class hierarchy.
+  template <typename T> class ReadOnlyOption : public BaseOption {
   public:
-    Option() = delete;
-    Option(Settings *s, const std::string &keyname, const T default_value)
+    ReadOnlyOption() = delete;
+    ReadOnlyOption(Settings *s, const std::string &keyname, const T default_value)
         : key_name(keyname), _value(default_value) {
 
-		if (s != nullptr) {
-			auto sres = s->_all_options.find(keyname);
-			if (sres == s->_all_options.end()) {
-				s->_all_options.emplace(keyname, this);
-			}
-			else {
-				THROW_EXCEPTION("Option duplicate key.");
-			}
-		}
+      if (s != nullptr) { /// register option in settings dict.
+        auto sres = s->_all_options.find(keyname);
+        if (sres == s->_all_options.end()) {
+          s->_all_options.emplace(keyname, this);
+        } else {
+          THROW_EXCEPTION("Option duplicate key.");
+        }
+      }
     }
 
     std::string key_str() const override { return key_name; }
-
     std::string value_str() const override { return std::to_string(_value); }
-
     void from_string(const std::string &s) override {
       std::istringstream iss(s);
       iss >> _value;
     }
+    /// return option value
+    T value() const { return _value; }
 
-	T value()const { return _value; }
-	void setValue(const T&value_) { 
-		logger_info("engine: change settings - ", this->key_name, " ", _value, " to ", value_);
-		_value = value_; 
-	}
-protected:
-	std::string key_name;
+  protected:
+    std::string key_name;
     T _value;
   };
 
+  template <typename T> class Option : public ReadOnlyOption<T> {
+  public:
+    Option() = delete;
+    Option(Settings *s, const std::string &keyname, const T default_value)
+        : ReadOnlyOption<T>(s, keyname, default_value) {}
+
+    void setValue(const T &value_) {
+      logger_info("engine: change settings - ", this->key_name, " ", _value, " to ",
+                  value_);
+      _value = value_;
+    }
+  };
+
 public:
-  EXPORT Settings(const std::string&storage_path);
+  EXPORT Settings(const std::string &storage_path);
   EXPORT ~Settings();
 
   EXPORT void set_default();
@@ -72,13 +85,13 @@ public:
   EXPORT std::vector<utils::async::ThreadPool::Params> thread_pools_params();
 
   EXPORT std::string dump();
-  EXPORT void change(std::string& expression);
-  
-  Option<std::string> storage_path;
-  Option<std::string> raw_path;
-  Option<std::string> bystep_path;
+  EXPORT void change(std::string &expression);
+
+  ReadOnlyOption<std::string> storage_path;
+  ReadOnlyOption<std::string> raw_path;
+  ReadOnlyOption<std::string> bystep_path;
   // aof level options;
-  Option<uint64_t> aof_max_size;  // measurements count in one file
+  Option<uint64_t> aof_max_size;    // measurements count in one file
   Option<uint64_t> aof_buffer_size; // inner buffer size
 
   Option<uint32_t> chunk_size;
@@ -86,31 +99,27 @@ public:
   Option<STRATEGY> strategy;
 
   // memstorage options;
-  Option<uint32_t> memory_limit; //in bytes;
-  Option<float>  percent_when_start_droping; //fill percent, when start dropping.
-  Option<float>  percent_to_drop; //how many chunk drop.
-  
-  bool  load_min_max; //if true - engine dont load min max. needed to ctl tool.
+  Option<uint32_t> memory_limit;            // in bytes;
+  Option<float> percent_when_start_droping; // fill percent, when start dropping.
+  Option<float> percent_to_drop;            // how many chunk drop.
+
+  bool load_min_max; // if true - engine dont load min max. needed to ctl tool.
 };
 
 using Settings_ptr = std::shared_ptr<Settings>;
 #ifndef MSVC
-template<>
-EXPORT std::string Settings::Option<STRATEGY>::value_str()const;
-template<>
-EXPORT std::string Settings::Option<std::string>::value_str()const;
+template <> EXPORT std::string Settings::ReadOnlyOption<STRATEGY>::value_str() const;
+template <> EXPORT std::string Settings::ReadOnlyOption<std::string>::value_str() const;
 #else
-template<>
-std::string Settings::Option<STRATEGY>::value_str()const {
-	std::stringstream ss;
-	ss << this->value();
-	return ss.str();
+template <> std::string Settings::ReadOnlyOption<STRATEGY>::value_str() const {
+  std::stringstream ss;
+  ss << this->value();
+  return ss.str();
 }
-template<>
-std::string Settings::Option<std::string>::value_str()const {
-	std::stringstream ss;
-	ss << this->value();
-	return ss.str();
+template <> std::string Settings::ReadOnlyOption<std::string>::value_str() const {
+  std::stringstream ss;
+  ss << this->value();
+  return ss.str();
 }
 #endif
 }
