8c8b39f0e0f8aeee25606ddb1b401ad30b00035c
Sergey Lyubimov <lysevi@gmail.com>
2016-12-28 13:07:31 +0300

    memstorage: clean chunk after stop.

--- libdariadb/storage/memstorage/memstorage.cpp
+++ libdariadb/storage/memstorage/memstorage.cpp
@@ -44,11 +44,6 @@ struct MemStorage::Private : public IMeasStorage, public MemoryChunkContainer {
   void stop() {
     if (!_stoped) {
       logger_info("engine: stop memstorage.");
-      for (size_t i = 0; i < _chunks.size(); ++i) {
-        _chunks[i] = nullptr;
-      }
-      _id2track.clear();
-
       _drop_stop = true;
       _drop_cond.notify_all();
       _drop_thread.join();
@@ -56,10 +51,16 @@ struct MemStorage::Private : public IMeasStorage, public MemoryChunkContainer {
         _crawler_cond.notify_all();
         _crawler_thread.join();
       }
+
 	  if (this->_down_level_storage != nullptr) {
 		  logger_info("engine: memstorage - drop all chunk to disk");
 		  this->drop_by_limit(1.0, true);
 	  }
+
+	  for (size_t i = 0; i < _chunks.size(); ++i) {
+		  _chunks[i] = nullptr;
+	  }
+	  _id2track.clear();
       _stoped = true;
 	  logger_info("engine: memstorage - stoped.");
     }
