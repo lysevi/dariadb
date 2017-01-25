5bc44398e4871799e4d515e0e78b8b35ace0de14
Sergey Lyubimov <lysevi@gmail.com>
2017-01-25 11:42:42 +0300

    clmn: leaf.

--- libdariadb/storage/clmn/nodes.h
+++ libdariadb/storage/clmn/nodes.h
@@ -2,8 +2,12 @@
 
 #include <libdariadb/meas.h>
 #include <libdariadb/st_exports.h>
+#include <libdariadb/storage/chunk.h>
 #include <libdariadb/utils/utils.h>
+#include <algorithm>
 #include <cstdint>
+#include <limits>
+#include <memory>
 
 namespace dariadb {
 namespace storage {
@@ -17,6 +21,7 @@ typedef uint32_t node_size_t;
 typedef uint64_t node_ptr;
 typedef uint8_t node_kind;
 
+const node_ptr NODE_PTR_NULL = std::numeric_limits<node_ptr>::max(); // for null pointers
 const node_kind NODE_KIND_FOOTER = 1;
 const node_kind NODE_KIND_ROOT = 1 << 1;
 const node_kind NODE_KIND_NODE = 1 << 2;
@@ -40,39 +45,97 @@ struct NodeHeader {
 };
 
 struct Footer {
+  typedef std::shared_ptr<Footer> Footer_Ptr;
+
   uint64_t magic_num; // for detect foother, when something going wrong
   NodeHeader hdr;
   uint32_t crc;
+  node_ptr *roots;
 
-  Footer() : hdr(0, 0, 0, NODE_KIND_FOOTER) { magic_num = MAGIC_NUMBER; }
+  Footer(node_size_t sz) : hdr(0, 0, sz, NODE_KIND_FOOTER) {
+    magic_num = MAGIC_NUMBER;
+	crc = 0;
+    roots = new node_ptr[hdr.size];
+    std::fill_n(roots, hdr.size, NODE_PTR_NULL);
+  }
+
+  ~Footer() { delete[] roots; }
+
+  static Footer_Ptr make_footer(node_size_t sz) { return std::make_shared<Footer>(sz); }
 
   // maximal id of child nodes.
   node_id_t max_node_id() const { return hdr.id; }
   EXPORT void update_crc();
 };
 
+struct Statistic {
+  Time min_time;
+  Time max_time;
+
+  Statistic() {
+    min_time = MAX_TIME;
+    max_time = MIN_TIME;
+  }
+  void update(const Meas &m) {}
+};
+
 struct Node {
+  typedef std::shared_ptr<Node> Node_Ptr;
   NodeHeader hdr;
+  Statistic stat;
+  node_ptr neighbor; // ptr to neighbor;
   Time *keys;
 
-  ~Node() {}
+  node_ptr *children;
 
-  static Node make_node(generation_t g, node_id_t _id, node_size_t sz) {
-    Node result{g, _id, sz, NODE_KIND_NODE};
-    return result;
-  }
-
-  static Node make_root(generation_t g, node_id_t _id, node_size_t sz) {
-    Node result{g, _id, sz, NODE_KIND_ROOT};
-    return result;
-  }
-
-protected:
   Node(generation_t g, node_id_t _id, node_size_t sz, node_kind _k)
-      : hdr(g, _id, sz, _k) {}
+      : hdr(g, _id, sz, _k), stat() {
+    keys = new Time[sz];
+    std::fill_n(keys, hdr.size, Time(0));
+
+	children = new node_ptr[sz];
+	std::fill_n(children, hdr.size, NODE_PTR_NULL);
+
+    neighbor = NODE_PTR_NULL;
+  }
+
+  ~Node() { delete[] keys; delete[] children; }
+
+  static Node_Ptr make_node(generation_t g, node_id_t _id, node_size_t sz) {
+    return std::make_shared<Node>(g, _id, sz, NODE_KIND_NODE);
+  }
+
+  static Node_Ptr make_root(generation_t g, node_id_t _id, node_size_t sz) {
+    return std::make_shared<Node>(g, _id, sz, NODE_KIND_ROOT);
+  }
+};
+
+struct Leaf {
+  typedef std::shared_ptr<Leaf> Leaf_Ptr;
+  NodeHeader hdr;
+  node_ptr neighbor; // ptr to neighbor;
+  ChunkHeader chunk_hdr;
+  uint8_t *chunk_buffer;
+
+  Leaf(generation_t g, node_id_t _id, node_size_t sz) : hdr(g, _id, sz, NODE_KIND_LEAF) {
+    chunk_buffer = new uint8_t[hdr.size];
+    std::fill_n(chunk_buffer, hdr.size, uint8_t(0));
+
+    neighbor = NODE_PTR_NULL;
+  }
+
+  ~Leaf() { delete[] chunk_buffer; }
+
+  static Leaf_Ptr make_leaf(generation_t g, node_id_t _id, node_size_t sz) {
+    return std::make_shared<Leaf>(g, _id, sz);
+  }
 };
 
 #pragma pack(pop)
+
+class NodeStorage {
+public:
+};
 }
 }
 }
--- tests/clmn_storage_test.cpp
+++ tests/clmn_storage_test.cpp
@@ -8,40 +8,66 @@
 using namespace dariadb::storage;
 
 BOOST_AUTO_TEST_CASE(Footer) {
-  char *expected = "dariadb.";
+  const char *expected = "dariadb.";
   auto number_representation = clmn::MAGIC_NUMBER;
   char *magic_str = (char *)&number_representation;
   for (size_t i = 0; i < sizeof(uint64_t); ++i) {
     BOOST_CHECK_EQUAL(magic_str[i], expected[i]);
   }
 
-  clmn::Footer ftr;
-  BOOST_CHECK_EQUAL(ftr.magic_num, clmn::MAGIC_NUMBER);
-  BOOST_CHECK_EQUAL(ftr.hdr.kind, clmn::NODE_KIND_FOOTER);
-  BOOST_CHECK_EQUAL(ftr.hdr.size, uint32_t(0));
-  BOOST_CHECK_EQUAL(ftr.hdr.gen, clmn::generation_t(0));
-  BOOST_CHECK_EQUAL(ftr.max_node_id(), clmn::node_id_t(0));
+  const clmn::node_size_t expected_s = 11;
+
+  clmn::Footer::Footer_Ptr ftr = clmn::Footer::make_footer(expected_s);
+  BOOST_CHECK_EQUAL(ftr->crc, uint32_t(0));
+  BOOST_CHECK_EQUAL(ftr->magic_num, clmn::MAGIC_NUMBER);
+  BOOST_CHECK_EQUAL(ftr->hdr.kind, clmn::NODE_KIND_FOOTER);
+  BOOST_CHECK_EQUAL(ftr->hdr.size, uint32_t(expected_s));
+  BOOST_CHECK_EQUAL(ftr->hdr.gen, clmn::generation_t(0));
+  BOOST_CHECK_EQUAL(ftr->max_node_id(), clmn::node_id_t(0));
+
+  for (clmn::node_size_t i = 0; i < expected_s; ++i) {
+    BOOST_CHECK_EQUAL(ftr->roots[i], clmn::NODE_PTR_NULL);
+  }
 }
 
-BOOST_AUTO_TEST_CASE(Node) {
-  clmn::generation_t expected_g = 1;
-  clmn::node_id_t expected_i = 2;
-  clmn::node_size_t expected_s = 3;
-  {
-    clmn::Node r = clmn::Node::make_root(expected_g, expected_i, expected_s);
+void checkNode(const clmn::Node::Node_Ptr &n, clmn::generation_t expected_g,
+               clmn::node_id_t expected_i, clmn::node_size_t expected_s) {
 
-    BOOST_CHECK_EQUAL(r.hdr.kind, clmn::NODE_KIND_ROOT);
-    BOOST_CHECK_EQUAL(r.hdr.gen, expected_g);
-    BOOST_CHECK_EQUAL(r.hdr.id, expected_i);
-    BOOST_CHECK_EQUAL(r.hdr.size, expected_s);
+  BOOST_CHECK_EQUAL(n->neighbor, clmn::NODE_PTR_NULL);
+  BOOST_CHECK_EQUAL(n->hdr.gen, expected_g);
+  BOOST_CHECK_EQUAL(n->hdr.id, expected_i);
+  BOOST_CHECK_EQUAL(n->hdr.size, expected_s);
+  for (clmn::node_size_t i = 0; i < expected_s; ++i) {
+    BOOST_CHECK_EQUAL(n->children[i], clmn::NODE_PTR_NULL);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(LeafAndNode) {
+  const clmn::generation_t expected_g = 1;
+  const clmn::node_id_t expected_i = 2;
+  const clmn::node_size_t expected_s = 3;
+
+  {
+    clmn::Node::Node_Ptr r = clmn::Node::make_root(expected_g, expected_i, expected_s);
+
+    BOOST_CHECK_EQUAL(r->hdr.kind, clmn::NODE_KIND_ROOT);
+    checkNode(r, expected_g, expected_i, expected_s);
   }
 
   {
-    clmn::Node n = clmn::Node::make_node(expected_g, expected_i, expected_s);
+    clmn::Node::Node_Ptr n = clmn::Node::make_node(expected_g, expected_i, expected_s);
 
-    BOOST_CHECK_EQUAL(n.hdr.kind, clmn::NODE_KIND_NODE);
-    BOOST_CHECK_EQUAL(n.hdr.gen, expected_g);
-    BOOST_CHECK_EQUAL(n.hdr.id, expected_i);
-    BOOST_CHECK_EQUAL(n.hdr.size, expected_s);
+    BOOST_CHECK_EQUAL(n->hdr.kind, clmn::NODE_KIND_NODE);
+    checkNode(n, expected_g, expected_i, expected_s);
+  }
+
+  {
+    clmn::Leaf::Leaf_Ptr l = clmn::Leaf::make_leaf(expected_g, expected_i, expected_s);
+
+    BOOST_CHECK_EQUAL(l->neighbor, clmn::NODE_PTR_NULL);
+    BOOST_CHECK_EQUAL(l->hdr.kind, clmn::NODE_KIND_LEAF);
+    BOOST_CHECK_EQUAL(l->hdr.gen, expected_g);
+    BOOST_CHECK_EQUAL(l->hdr.id, expected_i);
+    BOOST_CHECK_EQUAL(l->hdr.size, expected_s);
   }
 }
