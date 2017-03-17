e1eead0a996b62d25d603c4ee0f9aa3496a68d3c
Sergey Lyubimov <lysevi@gmail.com>
2017-03-17 08:07:01 +0300

    memstorage: _allocated is atomic.

--- tests/engine_test.cpp
+++ tests/engine_test.cpp
@@ -130,7 +130,7 @@ BOOST_AUTO_TEST_CASE(Engine_compress_all_test) {
       if (wals_count == 0) {
         break;
       }
-	  std::this_thread::sleep_for(std::chrono::milliseconds(300));
+	  std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
   }
   if (dariadb::utils::fs::path_exists(storage_path)) {
