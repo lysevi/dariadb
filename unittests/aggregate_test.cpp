9b3e68c9b6c4ca2c99f2ddf09cb2ac240ac369ae
Sergey Lyubimov <lysevi@gmail.com>
2017-05-01 20:16:56 +0300

    skeleton of aggregate test fixture.

--- unittests/aggregate_test.cpp
+++ unittests/aggregate_test.cpp
@@ -1,13 +1,25 @@
 #include <libdariadb/aggregate/aggregator.h>
 #include <libdariadb/aggregate/timer.h>
+#include <libdariadb/dariadb.h>
 #include <libdariadb/timeutil.h>
 #include <gtest/gtest.h>
 
 class Aggregate : public testing::Test {
 protected:
-  virtual void SetUp() {}
+  virtual void SetUp() {
+    _storage = dariadb::memory_only_storage();
+    _scheme = dariadb::scheme::Scheme::create(_storage->settings());
+  }
 
-  virtual void TearDown() {}
+  virtual void TearDown() {
+    _scheme = nullptr;
+    _storage = nullptr;
+  }
+
+  /*void fill_storage(dariadb::Time from, dariadb::Time to, dariadb::Time step) {
+  }*/
+  dariadb::IEngine_Ptr _storage;
+  dariadb::scheme::IScheme_Ptr _scheme;
 };
 
-
+TEST_F(Aggregate, Minute) {}
