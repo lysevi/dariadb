4e5089b8fbe1ce962bc55d82eaab6bda4dea0279
Sergey Lyubimov <lysevi@gmail.com>
2017-05-11 09:01:46 +0300

    MeasurementDescription output

--- libdariadb/scheme/helpers.cpp
+++ libdariadb/scheme/helpers.cpp
@@ -4,8 +4,18 @@ namespace dariadb {
 namespace scheme {
 
 std::ostream &operator<<(std::ostream &stream, const MeasurementDescription &d) {
-  stream << "{ name:" << d.name << ", id:" << d.id << "}";
+  stream << "{ name:" << d.name << ", id:" << d.id;
+
+  if (!d.interval.empty()) {
+    stream << ", interval:" << d.interval;
+  }
+
+  if (!d.aggregation_func.empty()) {
+    stream << ", func:" << d.aggregation_func;
+  }
+
+  stream << "}";
   return stream;
 }
-}
-}
+} // namespace scheme
+} // namespace dariadb
