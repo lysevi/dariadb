b5d4d06a3a817af7c9add1267febede048a02a62
Sergey Lyubimov <lysevi@gmail.com>
2017-07-03 12:05:16 +0300

    wal locks.

--- libdariadb/storage/wal/wal_manager.cpp
+++ libdariadb/storage/wal/wal_manager.cpp
@@ -629,20 +629,20 @@ void WALManager::flush_buffer_logic(const BufferDescription_Ptr &bd) {
   return;
 }
 
-void WALManager::flush_buffer(BufferDescription_Ptr &bd, bool sync) {
+void WALManager::flush_buffer(BufferDescription_Ptr &bd, bool /*sync*/) {
 	/*if (_buffer_pos == size_t(0)) {
 	  return;
 	}*/
-	AsyncTask at = [this, bd](const ThreadInfo &ti) {
-		TKIND_CHECK(THREAD_KINDS::DISK_IO, ti.kind);
+	/*AsyncTask at = [this, bd](const ThreadInfo &ti) {
+		TKIND_CHECK(THREAD_KINDS::DISK_IO, ti.kind);*/
 
 		flush_buffer_logic(bd);
-		return false;
+		/*return false;
 	};
 	auto handle = ThreadManager::instance()->post(THREAD_KINDS::DISK_IO, AT(at));
 	if (sync) {
 		handle->wait();
-	}
+	}*/
 }
 
 void WALManager::flush() {
